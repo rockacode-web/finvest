e7b722986d4d31be749be18a7c69ca991a669274
Rickon Dixon <rickondixon26@gmail.com>
2026-03-08 19:52:49 -0500

    Create Income entity

---
+++ src/main/java/com/example/finvest/entity/Income.java
@@ -0,0 +1,60 @@
+package com.example.finvest.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "income")
+public class Income {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String source;
+    private Double amount;
+    private String date;
+
+    public Income() {}
+
+    public Income(String source, Double amount, String date) {
+        this.source = source;
+        this.amount = amount;
+        this.date = date;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+}

a39197c9d649125c4a034425297bec8ae17ca31c
Rickon Dixon <rickondixon26@gmail.com>
2026-03-08 19:52:33 -0500

    Create Spring Boot main application class

---
+++ src/main/java/com/example/finvest/FinvestApplication.java
@@ -0,0 +1,12 @@
+package com.example.finvest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinvestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinvestApplication.class, args);
+    }
+}
